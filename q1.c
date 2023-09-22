f9790d841bc64d66a6bf7a7b4bd3b1bfb45d64ea
Prady009 <pradyumnpawar29092000@gmail.com>
2023-09-22 18:26:00 +0530

    Completed

---
+++ q1.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+// typedef struct Car
+// {
+//     int engine;
+//     int tire;
+//     int mai;
+// } Prady;
+
+// // struct Child a;
+// struct Child
+// {
+//     int Age;
+// };
+// struct Parent
+// {
+//     struct Child ch;
+// };
+
+// struct parent
+// {
+//     int parenttint;
+//     struct child
+//     {
+//         int childint;
+//     }c;
+// }p;
+    // typedef struct Prady
+    // {
+    //     int car;
+    //     struct Ansh
+    //     {
+    //         int bike;        
+    //     }; 
+    // }P; 
+
+    struct P{
+        int a;
+        int b; 
+        int c;         
+    
+    void acceptvalue(
+        printf("value:");
+        scanf("%d",&a);
+    
+    )
+    
+    };
+
+    void display(
+
+    )
+
+int main()
+{
+    // Prady P1 = {P1.engine = 1};
+    // P1.tire = 2;
+    // P1.mai = 3;
+    // printf("%d,%d,%d", P1.engine, P1.tire, P1.mai);
+
+    // struct Parent p;
+    // p.ch.Age = 20;
+
+   // struct parent p;
+    // p.c.childint=10;
+    // printf("%d",p.c.childint);
+
+    // P P1 = {P1.car=1};
+    // printf("%d",P1.car);
+    struct P P1;
+    acceptvalue(&P1);
+return 0;
+}
